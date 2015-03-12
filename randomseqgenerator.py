99ab734d765c6a377b62c9a84561b10c29df7b10
Richard Jansen <richard@lrtbl.nl>
2015-03-12 13:42:53 +0100

    Basics
    working

---
+++ randomseqgenerator.py
@@ -0,0 +1,21 @@
+from tkinter import *
+from tkinter import filedialog
+import random
+niets=Tk()
+file = filedialog.askopenfile()
+niets.destroy()
+niets.mainloop()
+
+raw_data = ""
+startReading = False
+for regel in file:
+    if startReading:
+        raw_data += regel[10:]
+    if "ORIGIN" in regel:
+        startReading = True
+sequence = raw_data.replace(' ','').replace('\n','').replace('\r','')
+
+y = len(sequence)
+d =[random.choice(sequence) for x in range(y)]
+s = "".join(d)
+print (s)
